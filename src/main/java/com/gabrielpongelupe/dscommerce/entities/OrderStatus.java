e7012eafe80acc379dcf93e89ada3b6fa6f1606f
GabrielPongelupe <gabriel.carvalho.1446145@sga.pucminas.br>
2025-02-09 19:45:19 -0300

    first commit

---
+++ src/main/java/com/gabrielpongelupe/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.gabrielpongelupe.dscommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    CANCELLED;
+
+
+}
---
+++ src/test/java/com/gabrielpongelupe/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielpongelupe.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
