cce539e3121f66f277ffee732e1f5c1442287085
GabrielPongelupe <gabrielpongelupee@gmail.com>
2025-03-25 22:50:26 -0300

    Exceptions Handler

---
+++ src/main/java/com/gabrielpongelupe/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.gabrielpongelupe.dscommerce.controllers.handlers;
+
+import com.gabrielpongelupe.dscommerce.dto.CustomError;
+import com.gabrielpongelupe.dscommerce.exceptions.DatabaseException;
+import com.gabrielpongelupe.dscommerce.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> databaseException(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.CONFLICT;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/gabrielpongelupe/dscommerce/services/ProductService.java
+++ src/main/java/com/gabrielpongelupe/dscommerce/services/ProductService.java
@@ -2,12 +2,17 @@ package com.gabrielpongelupe.dscommerce.services;
 
 import com.gabrielpongelupe.dscommerce.dto.ProductDTO;
 import com.gabrielpongelupe.dscommerce.entities.Product;
+import com.gabrielpongelupe.dscommerce.exceptions.DatabaseException;
+import com.gabrielpongelupe.dscommerce.exceptions.ResourceNotFoundException;
 import com.gabrielpongelupe.dscommerce.repositories.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -21,7 +26,9 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
-        Product product = this.productRepository.findById(id).get();
+        Product product = this.productRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
+
         return modelMapper.map(product, ProductDTO.class);
     }
 
@@ -39,19 +46,32 @@ public class ProductService {
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO productDTO) {
-        Product entity = productRepository.getReferenceById(id);
-        entity.setName(productDTO.getName());
-        entity.setDescription(productDTO.getDescription());
-        entity.setPrice(productDTO.getPrice());
-        entity.setImgUrl(productDTO.getImgUrl());
+        try{
+            Product entity = productRepository.getReferenceById(id);
+            entity.setName(productDTO.getName());
+            entity.setDescription(productDTO.getDescription());
+            entity.setPrice(productDTO.getPrice());
+            entity.setImgUrl(productDTO.getImgUrl());
 
-        entity = this.productRepository.save(entity);
-        return modelMapper.map(entity, ProductDTO.class); // Retorna o DTO atualizado
+            entity = this.productRepository.save(entity);
+            return modelMapper.map(entity, ProductDTO.class); // Retorna o DTO atualizado
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
-        this.productRepository.deleteById(id);
+        if (!productRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            productRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
     }
 
 
